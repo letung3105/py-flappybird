b4164624fc4b8ef603f6b9424baaa8315cd85fee
Tung Le <letung3105@gmail.com>
2018-10-13 02:25:33 +0700

    new documentation

--- player.py
+++ player.py
@@ -1,27 +1,32 @@
 import pygame
 
 
-ACCLERATION = 1
-FLAP_VELOCITY = -9
-MAX_VELOCITY = 10
+ACC = 1  # acceleration rate
+VEL_FLAP = -9  # flap instant velocity
+VEL_MAX = 10  # max velocity
 
 class Player():
+    '''holds images, updates position and performs player action'''
     def __init__(self, images, animation_pattern, x, y):
-        self.images = images
-        self.animation_pattern = animation_pattern
-        self.image = images[next(animation_pattern)]
-        self.rect = self.image.get_rect()
+        self.images = images  # images used for animation
+        self.animation_pattern = animation_pattern  # animation pattern
+        self.image = images[next(animation_pattern)]  # get first image
+        self.rect = self.image.get_rect()  # rectangle from image
+        # initial position
         self.rect.x = x
         self.rect.y = y
-        self.velocity = 0
+        self.velocity = VEL_FLAP
 
     def update(self):
-        if self.velocity < MAX_VELOCITY:
-            self.velocity += ACCLERATION
-        self.rect.y += self.velocity
+        '''update player position'''
+        if self.velocity < VEL_MAX:
+            self.velocity += ACC  # increase velocity for each update
+        self.rect.y += self.velocity  # change position based on velocity
 
     def animate(self):
+        '''change current image to the next in cycle'''
         self.image = self.images[next(self.animation_pattern)]
 
     def flap(self):
-        self.velocity = FLAP_VELOCITY
+        '''change player instant velocity'''
+        self.velocity = VEL_FLAP
